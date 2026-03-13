3ce5cd1560ba17c52cb3b76a04f02d2bad177189
Victor Lopez <victorlopezlopez93@gmail.com>
2026-03-13 21:54:38 +0000

    chore: initial project setup with Spring Boot 3.4 and Java 21

---
+++ src/main/java/com/victorlopez/gibfqualitymonitor/GibfQualityMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.victorlopez.gibfqualitymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibfQualityMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibfQualityMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorlopez/gibfqualitymonitor/GibfQualityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorlopez.gibfqualitymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GibfQualityMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
